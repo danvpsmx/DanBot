a66fbb1fce3e6d0022fe01802fcc539a8ff1c853
dan_bot <90216070+danvpsmx@users.noreply.github.com>
2021-09-10 05:03:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -127,7 +127,7 @@ cd usr
 cd etc
 rm -r bash.bashrc
 cd
-cd NyanBot
+cd DanBot
 mv bash.bashrc $HOME/../usr/etc
 echo -e '\033[0m\033[1;33m
    ____                         _                                        
